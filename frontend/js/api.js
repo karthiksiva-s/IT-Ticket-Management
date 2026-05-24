61110a49c62dec59df7e35c90a8fb50c52007eeb
karthiksiva-s <2k21cse063@kiot.ac.in>
2026-05-24 14:06:36 +0530

    updated html files

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -53,7 +53,7 @@ function requireAuth(requiredRole = null) {
 function logout() {
     localStorage.removeItem("token");
     localStorage.removeItem("user");
-    window.location.href = "../index.html";
+    window.location.href = "/pages/index.html";
 }
 
 // -------------------------------------------------------
